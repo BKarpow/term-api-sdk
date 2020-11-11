f8cc6f0846bbe46614ba2ce97ed5983c05c06f97
Bogdan <69055164+BKarpow@users.noreply.github.com>
2020-11-11 17:59:06 +0300

    0.03

--- term/term.py
+++ term/term.py
@@ -7,12 +7,25 @@ class Term(TermCore):
 
     def __init__(self):
         super(Term, self).__init__()
+        self.logging = True
         pass
 
     def BattaryStatus(self):
         cmd = self.getComands('battery')
         self.commandExecute(cmd)
-        logger.info('Command Ececute: ' + cmd)
+        
+
+    @logger.catch
+    def FingerPrint(self):
+        if self.logging:
+            logger.info('FingerPrint')
+        c = self.getComands('fingerptint')
+        res = self.commandExecute(c)
+        if len(res['errors']) == 0 and res['auth_status'] == "AUTH_RESULT_SUCCESS":
+            return (True, res['failed_attempts'])
+        else:
+            return (False, res['failed_attempts'], res['errors'])
+
 
 
 if __name__ == '__main__':
@@ -20,3 +33,4 @@ if __name__ == '__main__':
     TApi = Term()
     TApi.debug_trigger()
     TApi.BattaryStatus()
+    print(TApi.FingerPrint())
